791b0c67d307d29b604df26a87c801335ef7b7e7
Jefte31 <targinojefte@gmail.com>
2026-08-10 09:45:30 -0300

    Adiciona banco de palavras PT-BR

---
+++ words.js
@@ -0,0 +1,25 @@
+// Banco inicial de palavras em português do Brasil.
+// Mantido separado do motor do jogo para facilitar expansão futura.
+window.DIGITANDO_WORDS = {
+  easy: [
+    "asa", "ave", "azul", "bola", "bom", "café", "céu", "chuva", "cor", "doce",
+    "eco", "faro", "fogo", "gato", "giro", "hora", "ilha", "jogo", "lado", "lago",
+    "luz", "mar", "mesa", "mundo", "nave", "noite", "onda", "paz", "pipa", "riso",
+    "roda", "sal", "sol", "som", "tela", "terra", "vida", "vento", "voz", "zona"
+  ],
+  medium: [
+    "alegria", "amanhã", "amigo", "avião", "batalha", "beleza", "brasil", "caminho", "canção", "cidade",
+    "coração", "corrida", "criança", "desafio", "escola", "espaço", "estrela", "família", "feliz", "futuro",
+    "galáxia", "história", "janela", "jardim", "liberdade", "máquina", "missão", "música", "oceano", "planeta",
+    "praia", "rápido", "saúde", "segredo", "sonho", "tempo", "teclado", "universo", "viagem", "vitória"
+  ],
+  hard: [
+    "aceleração", "aprendizado", "brasileiro", "comunicação", "concentração", "conhecimento", "criatividade", "desenvolver",
+    "determinação", "eletricidade", "extraordinário", "habilidade", "imaginação", "independência", "inteligência", "maravilhoso",
+    "oportunidade", "persistência", "possibilidade", "programação", "responsável", "sustentável", "tecnologia", "velocidade"
+  ],
+  expert: [
+    "aperfeiçoamento", "autoconhecimento", "compatibilidade", "desenvolvimento", "extraordinariamente", "interdisciplinar",
+    "multidisciplinar", "responsabilidade", "sustentabilidade", "transformação", "ultraprocessado", "vulnerabilidade"
+  ]
+};
